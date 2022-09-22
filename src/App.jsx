724709674300834bfbb814a56b537658d31fd8a1
Bruno Benavent Colomo <70907703+brunobenavent@users.noreply.github.com>
2022-09-22 10:22:22 +0200

    subida del componente Header

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
-
+import Header from './components/Header'
 const App = () => {
   return (
     <div>
-      <p>hola mundo cruel</p>
+      <Header />
     </div>
   )
 }
